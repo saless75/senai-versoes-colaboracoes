d84a106762ea725cd56deeb9a077f27a257c9fd9
saless75 <mariaoliveira@apoioindaiatuba.local>
2021-09-20 18:24:40 -0300

    add logica

---
+++ logica prog/contagem.js
@@ -0,0 +1,10 @@
+var contagem = 80
+
+if (contagem >80) {
+    console.log("estoque alto")
+}else if(contagem <80){
+    console.log("estoque medio")
+} else {
+console.log("estoque baixo")
+}
+console.log("contagem")
