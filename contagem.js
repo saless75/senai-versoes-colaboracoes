130737f64b074d9c2018807d406baa02d1a7cfe8
saless75 <90013355+saless75@users.noreply.github.com>
2021-10-05 20:10:51 -0300

    Add files via upload

---
+++ contagem.js
@@ -0,0 +1,10 @@
+var quatidadedealunos = 14;
+
+for (var contador = 0; contador < quantidadedealunos; contador++){
+ if (contador == 0){
+ console.log("o numero é zero");
+ } 
+ else if(contador % 2 == 0){
+ console.log("contador + " o numero é par)  
+else
+}console.log(contador + " o numero é impar")
